b522160ee5ab18966ccc3a9555c8546625c1c695
Ashish-NSUT <ashishdubey0204@gmail.com>
2024-03-21 16:06:16 +0530

    login

--- src/components/Login/Signup.jsx
+++ src/components/Login/Signup.jsx
@@ -32,7 +32,7 @@ export default function Login() {
     }
     console.log("hi");
 
-    const response = await fetch("http://localhost:5000/api/auth/createuser",{
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/auth/createuser`,{
             method: "POST",
             headers: {
               "Content-Type": "application/json",
